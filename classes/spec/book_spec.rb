ec8264343752ffc363418fb442478a6ef4f986b2
brahimgb1team@gmail.com <brahimgb1team@gmail.com>
2022-08-17 21:36:12 +0000

    test class, should be an instance of Book

--- classes/spec/book_spec.rb
+++ classes/spec/book_spec.rb
@@ -5,5 +5,11 @@ describe Book do
   before :each do
     @book = Book.new('2022-09-09', false, 'Brahim', 'bad')
   end
+    describe 'Class test' do
 
+      it "should be an instance of Book" do
+        @book.should be_an_instance_of (Book)
+        expect(@book.class).to eq(Book)
+      end
+    end
 end
