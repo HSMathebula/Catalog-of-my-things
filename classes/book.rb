dfbdd51ba10e8a3fe525806e1aeff0a5efcda247
brahimgb1team@gmail.com <brahimgb1team@gmail.com>
2022-08-17 13:22:36 +0000

    require item.rb to set Item class parent of Book class

--- classes/book.rb
+++ classes/book.rb
@@ -1,6 +1,10 @@
+require_relative './item.rb'
 class Book < Item
-  def initialize(publisher,cover_state)
+  def initialize(publish_date, archived, publisher, cover_state)
+    super(publish_date, archived)
     @publisher = publisher
     @cover_state = cover_state
   end
+
+
 end
