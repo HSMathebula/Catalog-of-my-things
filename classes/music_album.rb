8fd1e4e5d13b1ad0cf3c8f1f4746af1ced96878c
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-08-17 12:37:40 +0200

    Create MusicAlbum class in a separate .rb file

---
+++ classes/music_album.rb
@@ -0,0 +1,15 @@
+require 'classes/item.rb'
+
+class MusicAlbum < Item
+  attr_accessor :id, :name, :on_spotify, :publish_date
+
+  def initialize(name, publish_date, on_spotufy)
+    super(id, publish_date)
+    @name = name
+    @on_spotify = on_spotify
+  end
+
+  def can_be_archived?
+    super && @on_spotify == true
+  end
+end
