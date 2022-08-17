d18e55d6031b83f76f824c14849cea81799417ac
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-08-17 12:44:39 +0200

    Rindomising id in music class

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -4,7 +4,8 @@ class MusicAlbum < Item
   attr_accessor :id, :name, :on_spotify, :publish_date
 
   def initialize(name, publish_date, on_spotufy)
-    super(id, publish_date)
+    super(publish_date)
+    @id = Random.rand(1...1000)
     @name = name
     @on_spotify = on_spotify
   end
