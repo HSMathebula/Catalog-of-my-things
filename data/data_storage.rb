8c73f3c0141b464d6b3c3065675015308d8d97da
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-08-18 21:22:57 +0200

    fix linter

--- data/data_storage.rb
+++ data/data_storage.rb
@@ -25,86 +25,84 @@ def load_games
   @games.each do |ga|
     puts "game multiplayer: #{ga.multiplayer}, last_played_at: #{ga.last_played_at}, publish_date: #{ga.publish_date}"
   end
-
+end
 
 def save_game(multiplayer, last_played_at, publish_date)
-    obj = {
-      multiplayer: multiplayer,
-      last_played_at: last_played_at,
-      publish_date: publish_date
-    }
+  obj = {
+    multiplayer: multiplayer,
+    last_played_at: last_played_at,
+    publish_date: publish_date
+  }
 
-    if File.exist?('./data/games.json')
-      file = File.open('./data/games.json')
+  if File.exist?('./data/games.json')
+    file = File.open('./data/games.json')
 
-      if file.size.zero?
-        game = [obj]
-      else
-        game = JSON.parse(File.read('./data/games.json'))
-        game << obj
-      end
-
-      file.close
-
-      myfile = File.open('./data/games.json', 'w')
-      myfile.write(JSON.pretty_generate(game))
-      myfile.close
-
+    if file.size.zero?
+      game = [obj]
     else
-      puts 'File empty. add new game.'
+      game = JSON.parse(File.read('./data/games.json'))
+      game << obj
     end
+
+    file.close
+
+    myfile = File.open('./data/games.json', 'w')
+    myfile.write(JSON.pretty_generate(game))
+    myfile.close
+
+  else
+    puts 'File empty. add new game.'
+  end
 end
 
 def load_authors
-    if File.exist?('./data/authors.json')
-      file = File.open('./data/authors.json')
-  
-      if File.empty?('./data/authors.json')
-        'Please add some authors '
-      else
-        authors = JSON.parse(File.read('./data/authors.json'))
-        authors.each do |author|
-            author = Author.new(author['first_name'],author['last_name'])
-          @authors << author
-        end
-      end
-      file.close
-    else
-      puts 'File empty. add new author.'
-    end
-    puts 'list of availble authors'
-    return if @authors.empty?
-  
-    @authors.each do |au|
-      puts "author first_name: #{au.first_name}, last_name: #{au.last_name}"
-    end
-end
+  if File.exist?('./data/authors.json')
+    file = File.open('./data/authors.json')
 
+    if File.empty?('./data/authors.json')
+      'Please add some authors '
+    else
+      authors = JSON.parse(File.read('./data/authors.json'))
+      authors.each do |author|
+        author = Author.new(author['first_name'], author['last_name'])
+        @authors << author
+      end
+    end
+    file.close
+  else
+    puts 'File empty. add new author.'
+  end
+  puts 'list of availble authors'
+  return if @authors.empty?
+
+  @authors.each do |au|
+    puts "author first_name: #{au.first_name}, last_name: #{au.last_name}"
+  end
+end
 
 def save_autor(first_name, last_name)
-    obj = {
-      first_name: first_name,
-      last_name: last_name,
-    }
+  obj = {
+    first_name: first_name,
+    last_name: last_name
+  }
 
-    if File.exist?('./data/authors.json')
-      file = File.open('./data/authors.json')
+  if File.exist?('./data/authors.json')
+    file = File.open('./data/authors.json')
 
-      if file.size.zero?
-        author = [obj]
-      else
-        author = JSON.parse(File.read('./data/authors.json'))
-        author << obj
-      end
-
-      file.close
-
-      myfile = File.open('./data/authors.json', 'w')
-      myfile.write(JSON.pretty_generate(author))
-      myfile.close
-
+    if file.size.zero?
+      author = [obj]
     else
-      puts 'File empty. add new author.'
+      author = JSON.parse(File.read('./data/authors.json'))
+      author << obj
     end
+
+    file.close
+
+    myfile = File.open('./data/authors.json', 'w')
+    myfile.write(JSON.pretty_generate(author))
+    myfile.close
+
+  else
+    puts 'File empty. add new author.'
+  end
 end
-  
